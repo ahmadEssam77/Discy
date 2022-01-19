207b2f799258606d86584324d3c8dc1aa88807ed
ahmadEssam77 <ahmad77essam@gmail.com>
2022-01-19 22:31:07 +0200

    start communities

--- src/views/Communities.vue
+++ src/views/Communities.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>This is Communities</div>
+  <div>
+    <div class="breadcrumb-wrapper">
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Library</li>
+        </ol>
+      </nav>
+    </div>
+    <main></main>
+  </div>
 </template>
 
 <script>
@@ -8,4 +18,18 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+div {
+  background-color: var(--white-text);
+}
+
+.breadcrumb-wrapper {
+  padding: 15px;
+  border-bottom: 2px solid var(--separator-color);
+}
+
+main {
+  background-color: var(--white-text);
+  height: 1355px;
+}
+</style>
